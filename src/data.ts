fb241b0209af044454909131a29b4d27451921ee
Daniel Abrante <danielabrante@protonmail.com>
2023-01-27 12:46:53 -0300

    Add new projects to data

--- src/data.ts
+++ src/data.ts
@@ -23,4 +23,18 @@ export const projects: ProjectProps[] = [
     img: `${BASE_URL_TO_IMAGES}/feed.png`,
     link: "https://feed-snowy-ten.vercel.app/",
   },
+  {
+    id: 2,
+    title: "Tribute Page",
+    description: "Biography about Dr. Norman Borlaug ",
+    img: `${BASE_URL_TO_IMAGES}/tribute-page.png`,
+    link: "https://danielabrante.github.io/tribute-page/",
+  },
+  {
+    id: 3,
+    title: "Todo App",
+    description: "A todo app to manage tasks",
+    img: `${BASE_URL_TO_IMAGES}/todo-app.png`,
+    link: "https://github.com/DanielAbrante/todo-web-p3",
+  },
 ];
